812172f7492c79054bc4f007ef9ec4ea3403314f
Sam Smith <smithrunner@live.com>
2017-10-26 10:16:51 -0500

    ball is moving and hitting paddles!

--- scripts/pong.js
+++ scripts/pong.js
@@ -12,6 +12,7 @@ canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 
+// Loads our canvas
 window.onload = function() {
   document.body.appendChild(canvas);
   animate(step);
@@ -24,8 +25,48 @@ var step = function() {
 };
 
 var update = function() {
+  ball.update(player.paddle, computer.paddle);
 };
 
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
+
+  if (this.x - 5 < 0) {
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 400) {
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
+
+  if(this.y < 0 || this.y > 600) {
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+  }
+
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  } else {
+    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  }
+};
+
+// Adds players and a ball
 var render = function() {
   context.fillStyle = "#FF00FF";
   context.fillRect(0, 0, width, height);
@@ -34,6 +75,7 @@ var render = function() {
   ball.render();
 };
 
+// Defines a paddle and coordinates
 function Paddle(x, y, width, height) {
   this.x = x;
   this.y = y;
@@ -43,14 +85,17 @@ function Paddle(x, y, width, height) {
   this.y_speed = 0;
 }
 
+// One paddle for the human
 function Player() {
   this.paddle = new Paddle(175, 580, 50, 10);
 }
 
+// One paddle for Skynet
 function Computer() {
   this.paddle = new Paddle(175, 10, 50, 10);
 }
 
+// One Ball to play with
 function Ball(x, y) {
   this.x = x;
   this.y = y;
@@ -59,12 +104,11 @@ function Ball(x, y) {
   this.radius = 5;
 }
 
+// Renders all the neccessary parts!
 Paddle.prototype.render = function() {
   context.fillStyle = "#0000FF";
   context.fillRect(this.x, this.y, this.width, this.height);
 };
-
-
 
 Player.prototype.render = function() {
   this.paddle.render();
@@ -73,8 +117,6 @@ Player.prototype.render = function() {
 Computer.prototype.render = function() {
   this.paddle.render();
 };
-
-
 
 Ball.prototype.render = function() {
   context.beginPath();
