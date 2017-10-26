3d1f6867188babdb509a7c80cef7e4fdea82f0dc
Sam Smith <smithrunner@live.com>
2017-10-26 12:01:33 -0500

    added comments and organized the code better

--- scripts/pong.js
+++ scripts/pong.js
@@ -1,8 +1,51 @@
-// I am using requestAnimationFrame to perform a callback request at 60fps
-var animate = window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function(callback) { window.setTimeout(callback, 1000/60)} ;
+//Hello! This is my version of the classic Pong game. I wrote this program following
+// the help of an article from https://robots.thoughtbot.com . I tried to make the code
+// my own but I must give credit to their tutorial for walking me through it. If you're reading This
+// and end up playing the game and can actually score against this stupid computer, kudos to you. I can't
+// beat this thing to save my life but my pride wont let me make the computer move slower.
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// LET"S DEFINE THE BASIC COMPONENTS ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// A Paddle
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
+}
+
+// One paddle for the human
+function Player() {
+  this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+// One paddle for Skynet
+function Computer() {
+  this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+// One Ball to play with
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 5;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// LET"S CREATE THE BASIC COMPONENTS ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// create some players and a ball
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(200, 300);
 
 // Create our "table" using a canvas
 var canvas = document.createElement('canvas');
@@ -12,10 +55,10 @@ canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 
-// create some players and a ball
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball(200, 300);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// LET"S DRAW THE SCORE ON THE BOARD ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 // start the scores at 0
 var computerScore = 0;
@@ -29,26 +72,9 @@ function drawScore() {
   context.fillText(computerScore, 186 , 150);
 }
 
-
-
-// Loads our canvas
-window.onload = function() {
-  document.body.appendChild(canvas);
-  animate(step);
-};
-
-//Create an object and listeners for when the human presses the arrow keys
-// to move the paddle
-var keysDown = {};
-
-window.addEventListener("keydown", function(event) {
-  keysDown[event.keyCode] = true;
-});
-
-window.addEventListener("keyup", function(event) {
-  delete keysDown[event.keyCode];
-});
-
+/////////////////////////////////////////////////////////////////////////////////////////
+////// LET"S DEFINE THE RENDER AND UPDATE FUNCTIONS FOR EACH COMPONENT //////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 var step = function() {
   update();
@@ -63,6 +89,42 @@ var update = function() {
   ball.update(player.paddle, computer.paddle);
 };
 
+// Adds players and a ball
+var render = function() {
+  context.fillStyle = "#1D4F36";
+  context.fillRect(0, 0, width, height);
+  player.render();
+  computer.render();
+  ball.render();
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// OUR RENDER FUNCTIONS ////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+Paddle.prototype.render = function() {
+  context.fillStyle = "#FFFFFF";
+  context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#8EB2A1";
+  context.fill();
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// OUR UPDATE FUNCTIONS ////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 Ball.prototype.update = function(paddle1, paddle2) {
   this.x += this.x_speed;
@@ -138,6 +200,12 @@ Computer.prototype.update = function(ball) {
   }
 };
 
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// OUR PADDLE MOVE FUNCTIONS ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+
 Paddle.prototype.move = function(x,y) {
   this.x += x;
   this.y += y;
@@ -153,61 +221,35 @@ Paddle.prototype.move = function(x,y) {
 }
 
 
-// Adds players and a ball
-var render = function() {
-  context.fillStyle = "#1D4F36";
-  context.fillRect(0, 0, width, height);
-  player.render();
-  computer.render();
-  ball.render();
+/////////////////////////////////////////////////////////////////////////////////////////
+////// A LISTENER EVENT FOR WHEN THE ARROW KEYS ARE PRESSED /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+//Create an object and listeners for when the human presses the arrow keys
+// to move the paddle
+var keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+////// LETS MAKE IT ALL HAPPEN BABY /////////////////////// /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Loads our canvas
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
 };
 
-// Defines a paddle and coordinates
-function Paddle(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
-}
-
-// One paddle for the human
-function Player() {
-  this.paddle = new Paddle(175, 580, 50, 10);
-}
-
-// One paddle for Skynet
-function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
-}
-
-// One Ball to play with
-function Ball(x, y) {
-  this.x = x;
-  this.y = y;
-  this.x_speed = 0;
-  this.y_speed = 3;
-  this.radius = 5;
-}
-
-// Renders all the neccessary parts!
-Paddle.prototype.render = function() {
-  context.fillStyle = "#FFFFFF";
-  context.fillRect(this.x, this.y, this.width, this.height);
-};
-
-Player.prototype.render = function() {
-  this.paddle.render();
-};
-
-Computer.prototype.render = function() {
-  this.paddle.render();
-};
-
-Ball.prototype.render = function() {
-  context.beginPath();
-  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
-  context.fillStyle = "#8EB2A1";
-  context.fill();
-};
+// I am using requestAnimationFrame to perform a callback request at 60fps
+var animate = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  function(callback) { window.setTimeout(callback, 1000/60)} ;
