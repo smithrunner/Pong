ddd48749fbcdf3bb22183d9e3e681d85f3eb1944
Sam Smith <smithrunner@live.com>
2017-10-26 23:28:16 -0500

    made the canvas dynamic

--- scripts/pong.js
+++ scripts/pong.js
@@ -8,6 +8,14 @@
 ////// LET"S DEFINE THE BASIC COMPONENTS ////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
+var tableWidth = window.innerWidth - 20;
+var tableHeight = window.innerHeight - 20;
+var tableCenterW = tableWidth / 2;
+var tableCenterH = tableHeight / 2;
+var ballRadius = tableWidth / 80;
+var paddleLength = tableWidth / 8;
+
+
 // A Paddle
 function Paddle(x, y, width, height) {
   this.x = x;
@@ -20,12 +28,12 @@ function Paddle(x, y, width, height) {
 
 // One paddle for the human
 function Player() {
-  this.paddle = new Paddle(175, 580, 50, 10);
+  this.paddle = new Paddle((tableWidth/2) - (paddleLength/2), tableHeight - 20, paddleLength, 10);
 }
 
 // One paddle for Skynet
 function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
+  this.paddle = new Paddle((tableWidth/2) - (paddleLength/2), 10, paddleLength, 10);
 }
 
 // One Ball to play with
@@ -34,7 +42,7 @@ function Ball(x, y) {
   this.y = y;
   this.x_speed = 0;
   this.y_speed = 3;
-  this.radius = 5;
+  this.radius = ballRadius;
 }
 
 
@@ -45,12 +53,12 @@ function Ball(x, y) {
 // create some players and a ball
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200, 300);
+var ball = new Ball(tableCenterW, tableCenterH);
 
 // Create our "table" using a canvas
 var canvas = document.createElement('canvas');
-var width = 400;
-var height = 600;
+var width = tableWidth;
+var height = tableHeight;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
@@ -68,8 +76,8 @@ var humanScore = 0;
 function drawScore() {
   context.font = "50px Monospace";
   context.fillstyle = "#0095DD";
-  context.fillText(humanScore, 186, 450);
-  context.fillText(computerScore, 186 , 150);
+  context.fillText(humanScore, tableCenterW - ballRadius, tableHeight - (paddleLength*2));
+  context.fillText(computerScore, tableCenterW - ballRadius, (paddleLength*2));
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -129,34 +137,34 @@ Ball.prototype.render = function() {
 Ball.prototype.update = function(paddle1, paddle2) {
   this.x += this.x_speed;
   this.y += this.y_speed;
-  var top_x = this.x - 5;
-  var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
-  var bottom_y = this.y + 5;
+  var top_x = this.x - ballRadius;
+  var top_y = this.y - ballRadius;
+  var bottom_x = this.x + ballRadius;
+  var bottom_y = this.y + ballRadius;
 
   if (this.x - 5 < 0) {
     this.x = 5;
     this.x_speed = -this.x_speed;
-  } else if(this.x + 5 > 400) {
-    this.x = 395;
+  } else if(this.x + 5 > tableWidth) {
+    this.x = tableWidth - ballRadius;
     this.x_speed = -this.x_speed;
   }
 
   if(this.y < 0) {
     this.x_speed = 0;
     this.y_speed = 3;
-    this.x = 200;
-    this.y = 300;
+    this.x = tableCenterW;
+    this.y = tableCenterH;
     humanScore++;
-  } else if(this.y > 600) {
+  } else if(this.y > tableHeight) {
     this.x_speed = 0;
     this.y_speed = 3;
-    this.x = 200;
-    this.y = 300;
+    this.x = tableCenterW;
+    this.y = tableCenterH;
     computerScore++;
   }
 
-  if(top_y > 300) {
+  if(top_y > tableCenterH) {
     if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
       this.y_speed = -3;
       this.x_speed += (paddle1.x_speed / 2);
@@ -195,8 +203,8 @@ Computer.prototype.update = function(ball) {
   this.paddle.move(diff, 0);
   if(this.paddle.x < 0) {
     this.paddle.x = 0;
-  } else if (this.paddle.x + this.paddle.width > 400) {
-    this.paddle.x = 400 - this.paddle.width;
+  } else if (this.paddle.x + this.paddle.width > tableWidth) {
+    this.paddle.x = tableWidth - this.paddle.width;
   }
 };
 
@@ -214,8 +222,8 @@ Paddle.prototype.move = function(x,y) {
   if(this.x < 0) {
     this.x = 0;
     this.x_speed = 0;
-  } else if (this.x + this.width > 400) {
-    this.x = 400 - this.width;
+  } else if (this.x + this.width > tableWidth) {
+    this.x = tableWidth - this.width;
     this.x_speed = 0;
   }
 }
